ca84463d1bbc3482bb8627548ee81e50f3cf93ea
Siyuan Peng <peng2000@vulcansub00.umiacs.umd.edu>
2023-04-21 00:12:01 -0400

    add vulcan train scirpt

---
+++ train.sh
@@ -0,0 +1,3 @@
+python main.py --batch_size 8 --model FlowNet2 --loss=L1Loss --optimizer=Adam --optimizer_lr=1e-4 \
+--training_dataset ChairsSDHomTrain --training_dataset_root /vulcanscratch/peng2000/ChairsSDHom \
+--validation_dataset ChairsSDHomTest --validation_dataset_root /vulcanscratch/peng2000/ChairsSDHom
